c490708a1101c6a6dd29d92627e85ad054b9bb01
richard <richardalanjones@gmail.com>
2023-03-15 22:29:29 -0700

    updated toggleterm config

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -13,7 +13,7 @@ toggleterm.setup {
 	shade_terminals = true,
 	shading_factor = 2,
 	start_in_insert = true,
-	insert_mappings = true,
+	--insert_mappings = true,
 	persist_size = true,
 	--direction = "vertical",
 	direction = "horizontal",
@@ -29,17 +29,20 @@ toggleterm.setup {
 	},
 }
 
-function _G.set_terminal_keymaps()
-	local buf_opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], buf_opts)
-	vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], buf_opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], buf_opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], buf_opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], buf_opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], buf_opts)
-end
+--function _G.set_terminal_keymaps()
+--	local opts = { buffer = 0 }
+--	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+--	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+--	vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+--	vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+--	vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+--	vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+--	vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+--end
+--
+---- if you only want these mappings for toggle term use term://*toggleterm#* instead
+--vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 -- General Mappings
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
@@ -51,25 +54,49 @@ local Terminal = require('toggleterm.terminal').Terminal
 --Julia REPL
 local julia = Terminal:new({ cmd = "julia", hidden = false })
 
-local function julia_repl()
+function _julia_repl()
 	julia:toggle()
 end
 
-map("n", "<leader>jr", [[<cmd>lua julia_repl()<CR> using Pkg<CR> Pkg.activate(".")<CR>]], opts)
+map("n", "<leader>jr", [[<cmd>lua _julia_repl()<CR> using Pkg<CR> Pkg.activate(".")<CR>]], opts)
 
 --Python REPL
 local python_env = Terminal:new({ cmd = "pipenv shell", hidden = false })
 
-local function wait(seconds)
+function Wait(seconds)
 	local start = os.time()
 	repeat
 	until os.time() > start + seconds
 end
 
-function python_repl()
+function _python_repl()
 	python_env:toggle()
-	wait(0.5)
+	Wait(0.5)
 	vim.cmd("TermExec cmd=python")
 end
 
-map("n", "<leader>pr", [[<cmd>lua python_repl()<CR>]], opts)
+map("n", "<leader>pr", [[<cmd>lua _python_repl()<CR>]], opts)
+
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		border = "double",
+	},
+	-- -- function to run on opening the terminal
+	-- on_open = function(term)
+	--   vim.cmd("startinsert!")
+	--   vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+	-- end,
+	-- -- function to run on closing the terminal
+	-- on_close = function(term)
+	--   vim.cmd("startinsert!")
+	-- end,
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+map("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
